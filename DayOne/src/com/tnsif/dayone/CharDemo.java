7ac69bf28a1c6f963b48bb075eab0d113c4c23ec
HP <HP@LAPTOP-REUQ2DKO>
2025-06-30 12:57:22 +0530

    Day 1

---
+++ DayOne/src/com/tnsif/dayone/CharDemo.java
@@ -0,0 +1,33 @@
+package com.tnsif.dayone;
+
+public class CharDemo {
+
+	public static void main(String[] args) {
+		
+		//assigning single character literal
+		
+		
+		char ch='a';
+		System.out.println(ch);
+		
+		//assigning num to char 
+		
+		char ch1=65;
+		
+		
+		System.out.println(ch1);
+		
+		int name='A';
+		
+		System.out.println(name);
+		
+		//assigning unicode to char
+		
+		char var1='\u20AC';
+		
+		System.out.println(var1);
+		
+
+	}
+
+}
---
+++ DayOne/src/com/tnsif/dayone/IdentifierDemo.java
@@ -0,0 +1,40 @@
+package com.tnsif.dayone;
+
+public class IdentifierDemo {
+
+	public static void main(String[] args) {
+		
+		
+		//variables declaration - invalid idenfier examples 
+		
+		//int for=13;  erroe bcoz keyword cannot be be declare as variables 
+		
+		//int num 1= 13;  erroe bcoz of space 
+		
+		//int @num =13; special are not allowed only $ _ ARE ALLOWED 
+		
+		
+		//valid identification
+		
+		
+		int $num1= 20;
+		
+		String StudentName="Heti";
+		
+		int FOR=12;
+		
+		System.out.println("Values of the number variables is "+ $num1);
+		
+		System.out.println("Student Name is : "+ StudentName);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
