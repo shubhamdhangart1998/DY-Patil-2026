3d02b447fd32687c55965cf271438c5f99b4f6b7
HP <HP@LAPTOP-REUQ2DKO>
2025-07-02 15:36:08 +0530

    Day 3

---
+++ DayThree/src/com/tnsif/daythree/EncapsulationDemo/EncapsulationDemo.java
@@ -0,0 +1,17 @@
+package com.tnsif.daythree.EncapsulationDemo;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		OopsConceptDemo obj=new OopsConceptDemo();
+		
+		obj.setName("Shubham");
+		obj.setAge(23);
+		obj.setSerailNum(1001);
+		
+		System.out.println(obj);
+	}
+
+}
