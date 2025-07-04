2f6f1a0b71bb6860b10b047fb5114dafbd444a96
HP <HP@LAPTOP-REUQ2DKO>
2025-07-04 11:56:12 +0530

    Final and Static

---
+++ DaySix/src/com/tnsif/daysix/finaldemo/FinalMethodClass.java
@@ -0,0 +1,18 @@
+package com.tnsif.daysix.finaldemo;
+
+public class FinalMethodClass {
+
+	//constructor 
+	public FinalMethodClass() {
+		System.out.println("this is a default constructor ");
+	}
+	
+	final int a=50;
+	 // final method 
+	final void show()
+	{
+		System.out.println("value of a "+ a);
+	}
+	
+	
+}
