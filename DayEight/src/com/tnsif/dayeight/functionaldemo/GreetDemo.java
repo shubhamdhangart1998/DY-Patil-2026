e08f33a562831fb96ef6a7c9360006b86231cccb
HP <HP@LAPTOP-REUQ2DKO>
2025-07-07 15:04:42 +0530

    Day 8

---
+++ DayEight/src/com/tnsif/dayeight/functionaldemo/GreetDemo.java
@@ -0,0 +1,12 @@
+package com.tnsif.dayeight.functionaldemo;
+
+public class GreetDemo implements GreetInterface {
+
+	@Override
+	public void greet() {
+		System.out.println("Good morning ----");
+		
+	}
+
+	
+}
